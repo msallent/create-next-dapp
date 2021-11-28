c84ef8b1bef8dcf2a6d66139544002a95c8a3f9a
Matias Sallent <mqsallent@gmail.com>
2021-11-28 17:19:21 -0300

    npx hardhat

---
+++ hardhat.config.ts
@@ -0,0 +1,39 @@
+import dotenv from 'dotenv';
+import { HardhatUserConfig, task } from 'hardhat/config';
+import '@typechain/hardhat';
+import 'hardhat-gas-reporter';
+import '@nomiclabs/hardhat-waffle';
+
+dotenv.config({ path: './.env.local' });
+
+task('accounts', 'Prints the list of accounts', async (_, hre) => {
+  const accounts = await hre.ethers.getSigners();
+
+  for (const account of accounts) {
+    console.log(account.address);
+  }
+});
+
+const config: HardhatUserConfig = {
+  solidity: '0.8.9',
+  networks: {
+    kovan: {
+      url: process.env.KOVAN_URL,
+      accounts: process.env.PRIVATE_KEY ? [process.env.PRIVATE_KEY] : [],
+    },
+  },
+  gasReporter: {
+    currency: 'USD',
+    enabled: process.env.REPORT_GAS !== undefined,
+    coinmarketcap: process.env.COINMARKETCAP_API_KEY,
+  },
+  paths: {
+    sources: './src',
+    tests: './src/tests',
+  },
+  typechain: {
+    outDir: './src/types',
+  },
+};
+
+export default config;
