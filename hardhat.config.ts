50293af75785abbee693732f1fc0cfda8cda9f38
Matias Sallent <mqsallent@gmail.com>
2022-01-15 14:07:59 -0300

    Add comments

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -5,8 +5,11 @@ import 'hardhat-gas-reporter';
 import '@nomiclabs/hardhat-waffle';
 import '@nomiclabs/hardhat-etherscan';
 
+// Load environment variables from the `.env.local` file
 dotenv.config({ path: './.env.local' });
 
+// This is a sample Hardhat task. To learn how to create your own go to
+// https://hardhat.org/guides/create-task.html
 task('accounts', 'Prints the list of accounts', async (_, hre) => {
   const accounts = await hre.ethers.getSigners();
 
@@ -15,6 +18,8 @@ task('accounts', 'Prints the list of accounts', async (_, hre) => {
   }
 });
 
+// You need to export an object to set up your config
+// Go to https://hardhat.org/config/ to learn more
 const config: HardhatUserConfig = {
   solidity: '0.8.9',
   networks: {
