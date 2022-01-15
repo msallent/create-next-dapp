4d6435e9522dcd0f146137784796b58c5f011500
Matias Sallent <mqsallent@gmail.com>
2022-01-15 19:40:35 -0300

    Move metadata to _app

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,36 @@
 import '../styles/globals.css';
+import Head from 'next/head';
 import type { AppProps } from 'next/app';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <title>Create Next dApp</title>
+        <html lang="en" />
+        <link rel="icon" href="/favicon.ico" />
+        <meta
+          name="description"
+          content="Dapp template powered by Next.js, TypeScript and Hardhat"
+        />
+        <meta
+          name="keywords"
+          content="nextjs, solidity, smart, contracts, blockchain, ethereum, hardhat, typescript, tailwind"
+        />
+        <meta property="og:title" content="Create Next dApp" />
+        <meta property="og:url" content="https://create-next-dapp.vercel.app/" />
+        <meta
+          property="og:description"
+          content="Dapp template powered by Next.js, TypeScript and Hardhat"
+        />
+        <meta property="og:type" content="website" />
+        <meta property="og:image" content="https://nextjs.org/static/twitter-cards/home.jpg" />
+        <meta property="og:image:alt" content="Create Next dApp" />
+        <meta name="twitter:card" content="summary_large_image" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
 
 export default MyApp;
