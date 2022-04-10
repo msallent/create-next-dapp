a253adff8cb87f82ee93c2b7c14548e0166681a6
msallent <mqsallent@gmail.com>
2022-04-10 15:23:59 -0300

    Regenerate types

--- src/types/contracts/index.ts
+++ src/types/contracts/index.ts
@@ -2,5 +2,5 @@
 /* tslint:disable */
 /* eslint-disable */
 export type { Greeter } from "./Greeter";
-
+export * as factories from "./factories";
 export { Greeter__factory } from "./factories/Greeter__factory";
